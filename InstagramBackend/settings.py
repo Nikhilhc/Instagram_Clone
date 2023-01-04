8838797377a2f491f8d1dabcee6af3a027615ac6
Nikhilhc <nikilhc@gmail.com>
2023-01-04 14:33:25 +0530

    updated

--- InstagramBackend/settings.py
+++ InstagramBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hksov($ciq%q(ba=1yu6fju^lde+j_bzfw88esa7if8pzo_yzo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['instagramclone.herokuapp.com','localhost', '127.0.0.1','*']
 
 
 # Application definition
