0d1c02f0415c76ae35a376f845416d2849d704fc
Nikhilhc <nikilhc@gmail.com>
2023-01-05 12:56:36 +0530

    added profilemodels,admin,urls

--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework import routers
+from .my_profile_views import profile
 
+
+
+urlpatterns = [
+    path('my_profile/<int:user_id>', profile),
+]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Instagram_Clone
-Instagram_Clone
