ac28b0ad218d30be63f00055b934075183029e9e
Nikhilhc <nikilhc@gmail.com>
2023-01-05 13:37:56 +0530

    added urls and models,serializers

--- InstagramBackend/settings.py
+++ InstagramBackend/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
-STATIFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_URL = '/staticfiles/'
+STATIFILES_DIRS = [os.path.join(BASE_DIR,"staticfiles")]
 
 
 # Default primary key field type
