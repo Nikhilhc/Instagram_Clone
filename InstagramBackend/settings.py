bec08aaf5cb1b8c33e6a2dcd7b7d0609a988c87b
Nikhilhc <nikilhc@gmail.com>
2023-01-05 20:26:06 +0530

    added postgres db connection

--- InstagramBackend/settings.py
+++ InstagramBackend/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'InstagramBackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Testdatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'Nikhil123',
+        'HOST': 'database-1.caubhyjllbom.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
