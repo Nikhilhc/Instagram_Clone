2187037ecc586b572f5dc57f2dcc1c72b0596760
Nikhilhc <nikilhc@gmail.com>
2023-01-05 13:46:39 +0530

    added urls and models,serializers

--- InstagramBackend/urls.py
+++ InstagramBackend/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backend/', include('BackendApi.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
