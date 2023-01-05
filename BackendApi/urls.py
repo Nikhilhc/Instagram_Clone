46d98df0966cceb331340c9eeb7e743b4c19d639
Nikhilhc <nikilhc@gmail.com>
2023-01-05 12:34:40 +0530

    reverted back

--- BackendApi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .my_profile_views import profile
-
-
-
-urlpatterns = [
-    path('my_profile/<int:user_id>', profile),
-]
