6f74bf08c86c2b7647358723c64e188746b995a6
Nikhilhc <nikilhc@gmail.com>
2023-01-05 13:16:28 +0530

    added urls and models,serializers

--- BackendApi/admin.py
+++ BackendApi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post,Like,Follower,Comment,Hashtag,User
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Follower)
+admin.site.register(Comment)
+admin.site.register(Hashtag)
